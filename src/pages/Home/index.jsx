41c028744d05f7880d5ebdffe5c91d4236cfaecc
CamilleStagiaire <100408607+CamilleStagiaire@users.noreply.github.com>
2023-08-22 18:11:39 +0200

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+function Home() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+       <h1>Bonjour</h1>
+       
+      </header>
+    </div>
+  );
+}
+
+export default Home;
